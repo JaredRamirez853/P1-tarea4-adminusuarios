19c48559f27d9f52d3b5fe2b80b797a5473ef3db
Jared Ramirez <20251207@itla.edu.do>
2025-11-25 19:01:03 -0400

    Correción al abrir la app

---
+++ app/src/main/java/com/example/tarea4/p1/app.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.tarea4.p1;
+
+import Formularios.Login;
+
+/**
+ *
+ * @author ramir
+ */
+public class app {
+        public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new Login().setVisible(true);
+        });
+    }
+}
